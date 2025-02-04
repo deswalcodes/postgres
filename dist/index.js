502099cc2a3c762cd79bc50992b00dede2cc3f29
deswalcodes <21cs2014@rgipt.ac.in>
2025-02-05 01:59:10 +0530

    learning transactions and joins

--- dist/index.js
+++ dist/index.js
@@ -23,9 +23,40 @@ app.post("/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* ()
     const username = req.body.username;
     const password = req.body.password;
     const email = req.body.email;
-    const response = yield pgClient.query("INSERT INTO users (username, email, password) VALUES ($1, $2, $3)", [username, email, password]);
+    const city = req.body.city;
+    const country = req.body.country;
+    const street = req.body.street;
+    const pincode = req.body.pincode;
+    try {
+        yield pgClient.query("BEGIN;");
+        const response = yield pgClient.query(`INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id`, [username, email, password]);
+        const user_id = response.rows[0].id;
+        const addressInsertQuery = yield pgClient.query(`INSERT INTO address (user_id,city,country,street,pincode) VALUES ($1, $2, $3, $4, $5)`, [user_id, city, country, street, pincode]);
+        yield pgClient.query("COMMIT;");
+        res.json({
+            message: "you have signed up"
+        });
+    }
+    catch (er) {
+        res.json({
+            message: "error while signing"
+        });
+    }
+}));
+app.get("/metadata", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = req.query.id;
+    const response1 = yield pgClient.query("SELECT username,email,id FROM users WHERE id = $1", [id]);
+    const response2 = yield pgClient.query("SELECT * FROM address WHERE user_id = $1", [id]);
     res.json({
-        message: "you have signed up"
+        user: response1.rows[0],
+        address: response2.rows[0]
+    });
+}));
+app.get("/better-metadata", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = req.query.id;
+    const response = yield pgClient.query("SELECT users.id,users.username,users.email,address.city,address.country,address.pincode FROM users JOIN address ON users.id = address.user_id WHERE users.id = $1", [id]);
+    res.json({
+        response: response.rows
     });
 }));
 app.listen(3001);
--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,29 @@ pgClient.connect();
 
 //@ts-ignore
 app.post("/signup",async (req,res) => {
-   try{
     const username = req.body.username;
     const password = req.body.password;
     const email = req.body.email;
+    const city = req.body.city;
+    const country = req.body.country;
+    const street = req.body.street;
+    const pincode = req.body.pincode;
+   try{
+   
+
+
+    await pgClient.query("BEGIN;")
     const response = await pgClient.query(
-        "INSERT INTO users (username, email, password) VALUES ($1, $2, $3)", 
+        `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id`, 
         [username, email, password]
       );
+      const user_id = response.rows[0].id;
+      const addressInsertQuery = await pgClient.query(
+        `INSERT INTO address (user_id,city,country,street,pincode) VALUES ($1, $2, $3, $4, $5)`, 
+        [user_id,city,country,street,pincode]
+      );
+
+    await pgClient.query("COMMIT;")
       
     res.json({
         message : "you have signed up"
@@ -23,10 +38,27 @@ app.post("/signup",async (req,res) => {
    }
    catch(er){
     res.json({
-        message : "error while signing up"
+        message : "error while signing"
     })
    }
 
 
+})
+app.get("/metadata",async (req,res)=> {
+    const id = req.query.id;
+    const response1 = await pgClient.query("SELECT username,email,id FROM users WHERE id = $1",[id])
+    const response2 = await pgClient.query("SELECT * FROM address WHERE user_id = $1",[id])
+    res.json({
+        user : response1.rows[0],
+        address : response2.rows[0]
+    })
+}) 
+app.get("/better-metadata",async (req,res)=>{
+    const id = req.query.id;
+    const response = await pgClient.query("SELECT users.id,users.username,users.email,address.city,address.country,address.pincode FROM users JOIN address ON users.id = address.user_id WHERE users.id = $1",[id])
+    res.json({
+        response : response.rows
+
+    })
 })
 app.listen(3001)
